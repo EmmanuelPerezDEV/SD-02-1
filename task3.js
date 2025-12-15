57719485a3a71407695c6234767df2b3265ac405
Emmanuel Perez <emmanperez.19@gmail.com>
2025-12-15 14:32:39 -0600

    Se corrigio un error en el task 3

--- task3.js
+++ task3.js
@@ -2,10 +2,10 @@ const prompt = require("prompt-sync")();
 const name = prompt("Emmanuel Perez");
 
 for (let I = 1; I <=  105; I++) {
-  if(I%3==0 && I%5==0 && I%7==0){
-    console.log("FizzBuzzWoof");
-  }
-    else if(I%3==0 && I%7==0){
+  //if(I%3==0 && I%5==0 && I%7==0){
+   // console.log("FizzBuzzWoof");
+ // }
+    if(I%3==0 && I%7==0){
     console.log("FizzWoof");
   }
      else if(I%5==0 && I%7==0){
